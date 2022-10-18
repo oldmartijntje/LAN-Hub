0e1f2634c70eb16c9814f507bbd702bc6f1ddd6d
oldmartijntje <oldmartijntje@gmail.com>
2022-10-18 20:05:10 +0200

    start of getting data

--- accountReader.py
+++ accountReader.py
@@ -1,1 +1,20 @@
 import accounts_omac
+
+configSettings = accounts_omac.configFileTkinter()
+data = accounts_omac.defaultConfigurations.defaultLoadingTkinter(configSettings)
+if data == False:
+  exit()
+
+splitString = '//CUT//'
+
+response = ''
+
+path = ''
+
+def getThis():
+  pass
+
+
+
+while True:
+  data = accounts_omac.saveAccount(data, configSettings)
