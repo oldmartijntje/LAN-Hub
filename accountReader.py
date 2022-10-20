461fdcb9792f009e92abd5b0f69580fc69046b19
oldmartijntje <oldmartijntje@gmail.com>
2022-10-20 20:15:56 +0200

    extra schematic

--- accountReader.py
+++ accountReader.py
@@ -5,11 +5,16 @@ data = accounts_omac.defaultConfigurations.defaultLoadingTkinter(configSettings)
 if data == False:
   exit()
 
-splitString = '//CUT//'
-
 response = ''
 
-path = ''
+# data request and response
+packet = {'KEY':'69','request':['meep','meep1','kaas'],'meep':{"path":['achievements','UNO2byMarjinIDK','oopsie Woopsie','message']}, 'meep1':{"path":['achievements','UNO2byMarjinIDK','oopsie Woopsie','title']}, 'kaas':{"path":['name']}}
+receipt = {'KEY':'69', 'request':['meep','meep1','kaas'], 'meep': {'response': '200', 'data': 'OOPSIE WOOPSIE!!\nUwU We made a fucky wucky!!'}, 'meep1': {'response': '404'}, 'meep1': {'response': '401'}}
+
+# server: give key when key used
+
+
+# backgroundcheck request and response
 
 def getThis():
   pass
